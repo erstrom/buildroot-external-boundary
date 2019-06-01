1e427da198eda68ad60902ae91b67b7d7d5ccd08
Erik Stromdahl <erik.stromdahl@gmail.com>
2019-06-01 19:18:36 +0200

    docker: add wrapper scripts for make inside container
    Convenience script used to create intermediate containers for building
    buildroot and all tools etc.
    The docker image (created from docker/ubuntu-16.04/Dockerfile) is
    assumed to have the name "brd"
    
    Signed-off-by: Erik Stromdahl <erik.stromdahl@gmail.com>

---
+++ docker-make.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+DOCKER_IMAGE="brd"
+
+# Make sure the image exists
+docker images | grep $DOCKER_IMAGE > /dev/null
+if [ $? -ne 0 ] ; then
+	echo "Missing docker image: $DOCKER_IMAGE"
+	exit 1
+fi
+
+docker run --rm  -v `pwd`:/home/user/build brd TERM=screen; make -C buildroot BR2_EXTERNAL=..
